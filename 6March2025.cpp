668010c3e5a63d92b528120ad84450004936b18d
prateek-dhanker <prateekdhanker039@gmail.com>
2025-03-06 20:41:24 +0530

    6March2025

---
+++ 6March2025.cpp
@@ -0,0 +1,28 @@
+// 2965. Find Missing and Repeated Values
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+            long long n = grid.size(), total = n * n,sum = 0, sqrSum = 0;
+    
+            for (int row = 0; row < n; ++row) {
+                for (int col = 0; col < n; ++col) {
+                    sum += grid[row][col];
+                    sqrSum +=
+                        static_cast<long long>(grid[row][col]) * grid[row][col];
+                }
+            }
+    
+            long long sumDiff = sum - total * (total + 1) / 2;
+            long long sqrDiff = sqrSum - total * (total + 1) * (2 * total + 1) / 6;
+    
+           
+            int repeat = (sqrDiff / sumDiff + sumDiff) / 2;
+            int missing = (sqrDiff / sumDiff - sumDiff) / 2;
+    
+            return {repeat, missing};
+        }
+    };
