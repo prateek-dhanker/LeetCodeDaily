c61205ed71f5e7cc35e5f01c1fae873478657314
prateek-dhanker <prateekdhanker039@gmail.com>
2025-03-09 22:47:22 +0530

    9March2023

---
+++ 9March2023.cpp
@@ -0,0 +1,34 @@
+// 3208. Alternating Groups II
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        int numberOfAlternatingGroups(vector<int>& colors, int k) {
+            for (int i = 0; i < k - 1; i++) {
+                colors.push_back(colors[i]);
+            }
+    
+            int length = colors.size();
+            int result = 0;
+            int left = 0, right = 1;
+    
+            while (right < length) {
+                if (colors[right] == colors[right - 1]) {
+                    left = right;
+                    right++;
+                    continue;
+                }
+    
+                right++;
+    
+                if (right - left < k) continue;
+    
+                result++;
+                left++;
+            }
+    
+            return result;
+        }
+    };
