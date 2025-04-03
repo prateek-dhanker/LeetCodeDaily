a76ae5581d068357f35c2c09676dbc1641ba937c
prateek-dhanker <prateekdhanker039@gmail.com>
2025-04-03 10:52:29 +0530

    3April2025

---
+++ 3April2025.cpp
@@ -0,0 +1,21 @@
+// 2874. Maximum Value of an Ordered Triplet II
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        long long maximumTripletValue(vector<int>& nums) {
+            int n = nums.size();
+    
+            long long maxA = nums[0], maxAB = LLONG_MIN, maxABC = 0;
+    
+            for(int j=1; j<n-1; j++){
+                maxAB = max(maxAB,maxA-nums[j]);
+                maxABC = max(maxABC, maxAB*nums[j+1]);
+                maxA = max(maxA,1LL*nums[j]);
+            }
+    
+            return maxABC;
+        }
+    };
