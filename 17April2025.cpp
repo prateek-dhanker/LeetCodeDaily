53cb89ed4e5ee6dd0de0d7c07713880f05a77444
prateek-dhanker <prateekdhanker039@gmail.com>
2025-04-17 10:49:28 +0530

    17April2025

---
+++ 17April2025.cpp
@@ -0,0 +1,20 @@
+// 2176. Count Equal and Divisible Pairs in an Array
+#include <bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+    public:
+        int countPairs(vector<int>& nums, int k) {
+            int n = nums.size(),cnt = 0;
+    
+            for(int i=0;i<n;i++){
+                for(int j= i+1;j<n;j++){
+                    if(nums[i]==nums[j] && (i*j)%k==0)
+                        cnt++;
+                }
+            }
+    
+            return cnt;
+        }
+    };
